dcb15a37fc15dd0cfd58b44e1a615de366b312dc
Mohammad Amin Storani <amin.storani@gmail.com>
2021-01-04 15:12:28 +0100

    OutPutting my name

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace MiniProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Amin Storani");
         }
     }
 }
