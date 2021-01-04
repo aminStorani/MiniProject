f5034683c2e1439cc136fc5fb4cdffad902d2bc9
Mohammad Amin Storani <amin.storani@gmail.com>
2021-01-04 16:53:51 +0100

    levelOne to fix two classes

--- Program.cs
+++ Program.cs
@@ -10,4 +10,30 @@ namespace MiniProject
             Console.WriteLine("Amin Storani");
         }
     }
+    public class LaptopComputers
+    {
+        public string purchaseDate { get; set; }
+        public double price { get; set; }
+        public string modelNames { get; set; }
+        public LaptopComputers(string purchaseD, double pris, string pruductName)
+        {
+            purchaseDate = purchaseD;
+            price = pris;
+            modelNames = pruductName;
+        }
+
+    }
+    class MobilePhones
+    {
+        public MobilePhones(string purchaseD, string modelName, double pris)
+        {
+            PurchaseDate = purchaseD;
+            ProductName = modelName;
+            Price = pris;
+
+        }
+        public string ProductName { get; set; }
+        public string PurchaseDate { get; set; }
+        public double Price { get; set; }
+    }
 }
